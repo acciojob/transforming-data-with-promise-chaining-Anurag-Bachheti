2f0518070e3f170f208cfb139b144d8e53bc3146
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-18 01:18:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-//your JS code here. If required.
+let button1 = document.getElementById("btn");
+
+button1.addEventListener("click", myFunction);
+
+function myFunction(){
+let numberStart = document.getElementById("ip").value;
+	return new Promise((resolve)=> {
+		setTimeout(()=> {
+			document.getElementById("output").innerHTML = `Result: ${numberStart}`;
+		resolve();
+		}, 2000)
+	})
+	.then(()=> {
+		return new Promise((resolve)=> {
+			let multiply = numberStart * 2;
+			setTimeout(()=> {
+				document.getElementById("output").innerHTML = `Result: ${multiply}`;
+				resolve(multiply)
+			}, 2000)
+		})
+	})
+	.then((num)=> {
+		return new Promise((resolve)=> {
+			let subtract = num - 3;
+			setTimeout(()=> {
+				document.getElementById("output").innerHTML = `Result: ${subtract}`;
+				resolve(subtract);
+			}, 1000)
+		});
+	})
+	.then((data)=> {
+		return new Promise((resolve)=> {
+			let divide = data / 2;
+			setTimeout(()=> {
+				document.getElementById("output").innerHTML = `Result: ${divide}`;
+				resolve(divide);
+			}, 1000)
+		});
+	})
+	.then((data)=> {
+		return new Promise((resolve)=> {
+			let divide = data + 10;
+			setTimeout(()=> {
+				document.getElementById("output").innerHTML = `Result: ${divide}`;
+				resolve(divide);
+			}, 1000)
+		});
+	})
+}
